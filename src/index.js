731e9789d272fdbde00aae912280297684ec1b7c
sabareeswaran M <139220237+sabarees15@users.noreply.github.com>
2023-08-18 14:46:42 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Home from './components/Home';
+
+
+const el = document.getElementById('root');
+const root = ReactDOM.createRoot(el);
+const App = () => {
+    return(
+        <div>
+            <Home />
+        </div>
+    );
+}
+root.render(<App />);
